901988c9bff41137a4baac50ebb3d3e1f642d7e3
Ares <diego.amachiflores@gmail.com>
2023-03-09 10:51:31 -0500

    01

---
+++ src/com/example/Main.java
@@ -0,0 +1,16 @@
+package com.example;
+
+public class Main {
+    public static void main(String[] args) {
+
+        int entero = 0;
+        System.out.println(entero);
+        long d = 9223372036854775807L;
+        System.out.println(d + 1 );
+        String letras = "aaaaaaa";
+        System.out.println(letras);
+        boolean bandera = true;
+        System.out.println(!bandera);
+
+    }
+}
